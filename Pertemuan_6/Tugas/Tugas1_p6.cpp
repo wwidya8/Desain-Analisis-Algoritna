08472c3a8c3e378c52c15b33525b1a5935666283
wwidya8 <150359383+wwidya8@users.noreply.github.com>
2024-04-21 14:08:54 +0700

    Tugas1_p6.cpp

--- Pertemuan_6/Tugas/Tugas1_p6.cpp
+++ Pertemuan_6/Tugas/Tugas1_p6.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 class Mahasiswa {
 protected:
-    string status;
     string nama;
     int umur;
     string jurusan;
@@ -13,26 +12,25 @@ protected:
 public:
     Mahasiswa() {}
 
-    Mahasiswa(string status, string nama, int umur, string jurusan, string fakultas)
-        : status(status), nama(nama), umur(umur), jurusan(jurusan), fakultas(fakultas) {}
+    Mahasiswa(string nama, int umur, string jurusan, string fakultas)
+        : nama(nama), umur(umur), jurusan(jurusan), fakultas(fakultas) {}
 
-    void input() {
-        cout << "Masukkan Status Mahasiswa : ";
-        getline(cin, status);
-        cout << "Masukkan Nama Mahasiswa : ";
-        cin.ignore(); // to clear the input buffer
-        getline(cin, nama);
-        cout << "Masukkan Usia Mahasiswa : ";
-        cin >> umur;
-        cout << "Masukkan Jurusan Mahasiswa : ";
-        cin.ignore(); // to clear the input buffer
-        getline(cin, jurusan);
-        cout << "Masukkan Fakultas Mahasiswa : ";
-        getline(cin, fakultas);
-    }
+void input1() {
+	
+	    cout << "Masukkan Nama Mahasiswa : ";
+	    getline(cin, nama);
+	    cout << "Masukkan Usia Mahasiswa : ";
+	    cin >> umur;
+	    cout << "Masukkan Jurusan Mahasiswa : ";
+	    cin.ignore(); // to clear the input buffer
+	    getline(cin, jurusan);
+	    cout << "Masukkan Fakultas Mahasiswa : ";
+	    getline(cin, fakultas);
+}
 
-    void printInfo() {
-        cout << "Status : " << status << endl;
+
+    void printInfo1() {
+
         cout << "Nama : " << nama << endl;
         cout << "Usia : " << umur << endl;
         cout << "Jurusan : " << jurusan << endl;
@@ -40,19 +38,43 @@ public:
     }
 };
 
+class StatusMhs : public Mahasiswa {
+private:
+    string status;
+
+public:
+    StatusMhs() {}
+
+    StatusMhs(string status, string nama, int umur, string jurusan, string fakultas)
+        : Mahasiswa(nama, umur, jurusan, fakultas) {}
+
+	void input2() {
+	    cout << "Masukkan Status Mahasiswa : ";
+	    getline(cin, status);
+	    Mahasiswa::input1(); // Panggil input1() dari kelas Mahasiswa
+	}
+
+
+    void printInfo2() {
+        
+        cout << "Status : " << status << endl;
+        Mahasiswa::printInfo1(); 
+    }
+};
+
 int main() {
-    Mahasiswa mahasiswa[5];
-    string status, nama, jurusan, fakultas;
+    StatusMhs mahasiswa[5];
+    string status, nama, jurusan, fakultas, prodi;
     int umur;
 
     for (int i = 0; i < 5; ++i) {
-        cout << "\nMasukkan data untuk mahasiswa ke-" << i+1 << endl << endl;
-        mahasiswa[i].input();
+        cout << "\nMasukkan data untuk mahasiswa ke-" << i+1 <<endl << endl;
+        mahasiswa[i].input2();
     }
 
     cout << "\nData Mahasiswa:" << endl;
     for (int i = 0; i < 5; ++i) {
-        mahasiswa[i].printInfo();
+        mahasiswa[i].printInfo2();
         cout << endl;
     }
 
